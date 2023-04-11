54bc7cbe05f778cc151b370d975318d9594dd4d5
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-11 12:14:54 +0300

    Implemented the Users router to facilitate logging in

---
+++ backend/src/routers/user.router.ts
@@ -0,0 +1,40 @@
+/* This code is setting up a POST endpoint at "/api/users/login" that handles user login requests. It
+extracts the email and password from the request body using destructuring assignment. It then
+searches for a user in the `sampleUsers` array that matches the email and password provided. If a
+matching user is found, it generates a JSON Web Token (JWT) using the `jsonwebtoken` library and
+sends a response containing the user object with the JWT token added. If no matching user is found,
+it sends a 400 status code with an error message indicating that the email or password is invalid.
+The `import` statements at the beginning of the code are importing necessary modules and data. */
+import { Router } from "express";
+import { sampleUsers } from "../data";
+import Jwt from "jsonwebtoken";
+
+const router = Router();
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = sampleUsers.find((user) => {
+    return user.email == email && user.password == password;
+  });
+
+  user
+    ? res.send(generateTokenResponse(user))
+    : res.status(400).send("Email or Password is invalid");
+});
+
+/**
+ * The function generates a token response for a user with a specific expiration time.
+ * @param {any} user - The user parameter is an object that contains information about a user, such as
+ * their email and whether or not they are an admin.
+ * @returns The function `generateTokenResponse` is returning the `user` object with an added `token`
+ * property.
+ */
+const generateTokenResponse = (user: any) => {
+  const token = Jwt.sign(
+    { email: user.email, isAdmin: user.isAdmin },
+    "ThisIsMyPrivateKey",
+    { expiresIn: "30d" }
+  );
+  user.token = token;
+  return user;
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,9 @@
 /* This is a TypeScript code that creates an express application and sets up various endpoints for
 handling HTTP requests. It also uses the `cors` middleware to allow cross-origin requests from a
-specific origin. Additionally, it uses the `jsonwebtoken` library to generate a token for user
-authentication. The code defines routes for getting food items, searching for food items, getting
-food tags, getting food items by tag, getting a specific food item by ID, and logging in a user.
-Finally, it listens on port 5000 for incoming requests. */
+specific origin. Finally, it listens on port 5000 for incoming requests. The code also mounts the
+food router at the "/api/foods" path and sets up another endpoint for users at "/api/users". */
 import express from "express";
 import cors from "cors";
-import { sampleUsers } from "./data";
-import Jwt from "jsonwebtoken";
 import foodRouter from "./routers/food.router";
 
 // Create an express application
@@ -30,68 +26,7 @@ app.use(
 
 // Mount the food router at the "/api/foods" path
 app.use("/api/foods", foodRouter);
-
-// app.get("/api/foods", (req, res) => {
-//   res.send(sampleFoods);
-// });
-
-// app.get("/api/foods/search/:searchTerm", (req, res) => {
-//   const searchTerm = req.params.searchTerm;
-//   const foods = sampleFoods.filter((food) =>
-//     food.name.toLowerCase().includes(searchTerm.toLowerCase())
-//   );
-//   res.send(foods);
-// });
-
-// app.get("/api/foods/tags", (req, res) => {
-//   res.send(sampleTags);
-// });
-
-// app.get("/api/foods/tag/:tagName", (req, res) => {
-//   const tagName = req.params.tagName;
-//   const foods = sampleFoods.filter((food) => food.tags?.includes(tagName));
-//   res.send(foods);
-// });
-
-// app.get("/api/foods/:foodId", (req, res) => {
-//   const foodId = req.params.foodId;
-//   const foods = sampleFoods.find((food) => food.id == foodId)!;
-//   res.send(foods);
-// });
-
-/* This code sets up a POST endpoint at "/api/users/login" that handles user login requests. It first
-extracts the email and password from the request body using destructuring assignment. It then
-searches for a user in the `sampleUsers` array that matches the email and password provided. If a
-matching user is found, it generates a JSON Web Token (JWT) using the `jsonwebtoken` library and
-sends a response containing the user object with the JWT token added. If no matching user is found,
-it sends a 400 status code with an error message indicating that the email or password is invalid. */
-app.post("/api/users/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = sampleUsers.find((user) => {
-    return user.email == email && user.password == password;
-  });
-
-  user
-    ? res.send(generateTokenResponse(user))
-    : res.status(400).send("Email or Password is invalid");
-});
-
-/**
- * The function generates a token response for a user with a specific expiration time.
- * @param {any} user - The user parameter is an object that contains information about a user, such as
- * their email and whether or not they are an admin.
- * @returns The function `generateTokenResponse` is returning the `user` object with an added `token`
- * property.
- */
-const generateTokenResponse = (user: any) => {
-  const token = Jwt.sign(
-    { email: user.email, isAdmin: user.isAdmin },
-    "ThisIsMyPrivateKey",
-    { expiresIn: "30d" }
-  );
-  user.token = token;
-  return user;
-};
+app.use("/api/users", foodRouter);
 
 const port = 5000;
 app.listen(port, () => {
