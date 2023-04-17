59ddcd6524d144e4a99cdae1f730c4a7c8f9b901
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-17 11:00:15 +0300

    Designed the order items table and updated the css

--- frontend/src/app/components/partials/order-items-list/order-items-list.component.ts
+++ frontend/src/app/components/partials/order-items-list/order-items-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Order } from "src/app/shared/models/Order";
 
 @Component({
-  selector: 'app-order-items-list',
-  templateUrl: './order-items-list.component.html',
-  styleUrls: ['./order-items-list.component.css']
+  selector: "order-items-list",
+  templateUrl: "./order-items-list.component.html",
+  styleUrls: ["./order-items-list.component.css"],
 })
-export class OrderItemsListComponent {
+export class OrderItemsListComponent implements OnInit {
+  @Input() order!: Order;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
