9ea6bad01c02a37665ecf3035d3ca8542ed56152
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-17 16:30:57 +0300

    Re-implemented some new sample data to match the new fields i require

---
+++ backend/src/data.ts
@@ -0,0 +1,14 @@
+export const sampleUsers = [
+  {
+    id: "1",
+    name: "John Doe",
+    phoneNumber: "+254712345678",
+    password: "1234",
+  },
+  {
+    id: "2",
+    name: "Jane Doe",
+    phoneNumber: "+254787654321",
+    password: "1234",
+  },
+];
