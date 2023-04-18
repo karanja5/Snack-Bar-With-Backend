76e9d49ce3893841370a706f2149c0f9e74fc701
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-18 15:52:59 +0300

    Added the getNewOrderForCurrentUser method

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from "@angular/core";
 import { Order } from "../shared/models/Order";
 import { HttpClient } from "@angular/common/http";
-import { ORDER_CREATE_URL } from "../shared/constants/urls";
+import {
+  NEW_ORDER_FOR_CURRENT_USER_URL,
+  ORDER_CREATE_URL,
+} from "../shared/constants/urls";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -10,5 +14,9 @@ export class OrderService {
   constructor(private http: HttpClient) {}
   create(order: Order) {
     return this.http.post<Order>(ORDER_CREATE_URL, order);
+  }
+
+  getNewOrderForCurrentUser(): Observable<Order> {
+    return this.http.get<Order>(NEW_ORDER_FOR_CURRENT_USER_URL);
   }
 }
