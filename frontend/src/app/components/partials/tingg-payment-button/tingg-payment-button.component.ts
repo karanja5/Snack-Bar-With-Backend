f130833562c0deda1cd6c3073a8d65ba210f05b9
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-19 12:12:36 +0300

    Generated the payment button

---
+++ frontend/src/app/components/partials/tingg-payment-button/tingg-payment-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "tingg-payment-button",
+  templateUrl: "./tingg-payment-button.component.html",
+  styleUrls: ["./tingg-payment-button.component.css"],
+})
+export class TinggPaymentButtonComponent {}
