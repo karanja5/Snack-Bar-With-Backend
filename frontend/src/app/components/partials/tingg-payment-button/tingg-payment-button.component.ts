e88bd74e7e8a94d7b403ed57e094fad6176a416b
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-20 08:34:47 +0300

    Deleted unused components

--- frontend/src/app/components/partials/tingg-payment-button/tingg-payment-button.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "tingg-payment-button",
-  templateUrl: "./tingg-payment-button.component.html",
-  styleUrls: ["./tingg-payment-button.component.css"],
-})
-export class TinggPaymentButtonComponent {}
