ae2563a51a6933fc4b51cb1d5abbc8fb9e3c1af4
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-20 08:36:40 +0300

    Added the email and paymentId properties to the order model

--- frontend/src/app/shared/models/Order.ts
+++ frontend/src/app/shared/models/Order.ts
@@ -5,7 +5,9 @@ export class Order {
   items!: CartItem[];
   totalPrice!: number;
   name!: string;
+  email!: string;
   phoneNumber!: string;
+  paymentId!: string;
   createdAt!: Date;
   status!: string;
 }
