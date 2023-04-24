ca92e5ac2a0d3aa20c3ee107ef206046b9ed15a6
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-24 14:14:26 +0300

    Changed the structure of the if statement

--- backend/src/middlewares/auth.mid.ts
+++ backend/src/middlewares/auth.mid.ts
@@ -5,9 +5,9 @@ export default (req: any, res: any, next: any) => {
   /*This access_token is the name chosen for the token that is set 
   on an interceptor on the frontend side*/
   const token = req.headers.access_token as string;
-  if (!token) {
+  if (!token)
     return res.status(UNAUTHORIZED).send("Access denied. No token provided.");
-  }
+
   try {
     const decodedUserFromToken = verify(token, process.env.JWT_SECRET!);
     req.user = decodedUserFromToken;
