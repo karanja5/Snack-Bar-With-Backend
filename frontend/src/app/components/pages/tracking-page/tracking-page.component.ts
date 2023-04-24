1be49029058ca55cc961d990fe930023bb28e1c4
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-04-24 16:36:54 +0300

    Generated the tracking component

---
+++ frontend/src/app/components/pages/tracking-page/tracking-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+import { Order } from "src/app/shared/models/Order";
+import { ActivatedRoute } from "@angular/router";
+import { OrderService } from "../../../services/order.service";
+
+@Component({
+  selector: "app-tracking-page",
+  templateUrl: "./tracking-page.component.html",
+  styleUrls: ["./tracking-page.component.css"],
+})
+export class TrackingPageComponent implements OnInit {
+  order!: Order;
+  constructor(activatedRoute: ActivatedRoute, orderService: OrderService) {
+    const params = activatedRoute.snapshot.params;
+    if (!params.OrderId) {
+      return;
+    }
+    orderService.trackOrderById(params.OrderId).subscribe((order) => {
+      this.order = order;
+    });
+  }
+
+  ngOnInit(): void {}
+}
