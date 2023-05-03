0d2a03f748a00332eb160b890ca035dc51123975
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-05-03 15:30:14 +0300

    Enabled favoriting and its state persisted in local storage

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -10,9 +10,6 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { CartService } from "src/app/services/cart.service";
 import { FoodService } from "src/app/services/food.service";
 import { Food } from "src/app/shared/models/food";
-
-const FAV_KEY = "Favorite";
-
 @Component({
   selector: "app-food-page",
   templateUrl: "./food-page.component.html",
@@ -21,13 +18,13 @@ const FAV_KEY = "Favorite";
 /* The FoodPageComponent class retrieves a specific food item from a food service and allows the user
 to add it to their cart. */
 export class FoodPageComponent implements OnInit {
-  localFavorite: boolean = false;
   /* `food!: Food;` is declaring a property named `food` of type `Food`. The exclamation mark (`!`) is
   a non-null assertion operator, which tells TypeScript that the `food` property will be initialized
   at some point before it is used, even though it is not initialized in the constructor or in the
   `ngOnInit` method. This is a way to tell TypeScript that we are sure that the property will not be
   null or undefined at runtime, even though it is not initialized at compile time. */
   food!: Food;
+  favKey!: string;
   /**
    * This is a constructor function that initializes some services and subscribes to a route parameter
    * to get a specific food item by its ID.
@@ -44,6 +41,7 @@ export class FoodPageComponent implements OnInit {
    * provides methods for navigating to a specific route, navigating back to the previous route, and
    * navigating to a route with optional parameters.
    */
+
   constructor(
     activatedRoute: ActivatedRoute,
     foodService: FoodService,
@@ -61,10 +59,23 @@ export class FoodPageComponent implements OnInit {
       function inside the `subscribe` method is executed, which assigns the emitted `Food` object to
       the `food` property of the `FoodPageComponent` class. Essentially, this code block is
       retrieving a specific food item from the `foodService` based on its ID, and assigning it to
-      the `food` property of the component. */
+      the `food` property of the component. Additionally, it sets the `favKey` property to the name
+      of the food item, retrieves the favorite state of the food item from local storage, and sets
+      the `favorite` property of the `food` object based on the retrieved value. If the favorite
+      state is not found in local storage, it is set to `false`. */
       if (params.id) {
         foodService.getFoodById(params.id).subscribe((gottenFood) => {
           this.food = gottenFood;
+          this.favKey = this.food.name;
+          const favJson = localStorage.getItem(this.favKey);
+          if (!favJson) {
+            localStorage.setItem(this.favKey, JSON.stringify(false));
+          } else {
+            this.food.favorite = JSON.parse(favJson);
+          }
+          // this.food.favorite = favJson
+          //   ? JSON.parse(favJson)
+          //   : localStorage.setItem(this.favKey, JSON.stringify(false));
         });
       }
     });
@@ -72,9 +83,6 @@ export class FoodPageComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  /**
-   * The function adds a food item to the cart and navigates to the cart page.
-   */
   addToCart(): void {
     this.cartService.addToCart(this.food);
     this.router.navigateByUrl("/cart-page");
@@ -86,24 +94,25 @@ export class FoodPageComponent implements OnInit {
       window.scrollTo({ top, behavior: "smooth" });
     }
   }
+
+  /* This code block is toggling the favorite state of a food item and storing it in local storage.
+    It first retrieves the current favorite state of the food item from local storage using the
+    `localStorage.getItem` method and assigns it to the `favState` variable. If the `favState`
+    variable is equal to the string `"true"`, it sets the `favorite` property of the `food` object
+    to `false`, indicating that the food item is no longer a favorite. It then stores the updated
+    favorite state in local storage using the `localStorage.setItem` method. If the `favState`
+    variable is not equal to the string `"true"`, it sets the `favorite` property of the `food`
+    object to `true`, indicating that the food item is now a favorite, and stores the updated
+    favorite state in local storage. This code block allows the user to toggle the favorite state of
+    a food item and persist the state across different sessions using local storage. */
   toggleFavorite() {
-    this.food.favorite = !this.food.favorite;
-    this.localFavorite = this.food.favorite;
-    this.saveFavToLocalStorage(this.food.favorite);
+    const favState = localStorage.getItem(this.favKey);
+    if (favState === "true") {
+      this.food.favorite = false;
+      localStorage.setItem(this.favKey, JSON.stringify(this.food.favorite));
+    } else {
+      this.food.favorite = true;
+      localStorage.setItem(this.favKey, JSON.stringify(this.food.favorite));
+    }
   }
-  // toggleFavorite2() {
-  //   this.localFavorite = !this.localFavorite
-  //   this.saveFavToLocalStorage(this.localFavorite );
-  // }
-
-  private saveFavToLocalStorage = (bool: boolean) => {
-    localStorage.setItem(FAV_KEY, JSON.stringify(bool));
-  };
-
-  // private getFavFromLocalStorage = () => {
-  //   const fav = localStorage.getItem(FAV_KEY);
-  //   if (fav) {
-  //     this.localFavorite = JSON.parse(fav);
-  //   }
-  // };
 }
