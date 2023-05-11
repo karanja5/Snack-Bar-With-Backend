c54d41bce5ffa2c21eb263cf6b7dd8deb23e3967
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-05-11 16:50:10 +0300

    Rendered the tingg payment button

--- frontend/src/app/components/partials/tingg-payment-button/tingg-payment-button.component.ts
+++ frontend/src/app/components/partials/tingg-payment-button/tingg-payment-button.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Order } from "src/app/shared/models/Order";
-// import { Order } from "../../../shared/models/Order";
-// import { IOrder } from "../../../../../../backend/src/models/order.model";
-// import { Router } from "@angular/router";
-// import { OrderService } from "../../../services/order.service";
-
+import { CHECKOUT_ENCRYPTION_URL } from "src/app/shared/constants/urls";
+import { ToastrService } from "ngx-toastr";
+// import { OrderService } from "src/app/services/order.service";
+declare var Tingg: any;
 @Component({
   selector: "app-tingg-payment-button",
   templateUrl: "./tingg-payment-button.component.html",
@@ -12,6 +11,44 @@ import { Order } from "src/app/shared/models/Order";
 })
 export class TinggPaymentButtonComponent implements OnInit {
   @Input() order!: Order;
-  constructor() {}
-  ngOnInit() {}
+  constructor(private toastrService: ToastrService) {}
+
+  renderTinggButton() {
+    Tingg.renderPayButton({
+      text: "Pay with Tingg",
+      color: "purple",
+      className: "awesome-checkout-button",
+    });
+    (document.querySelector(
+      ".awesome-checkout-button"
+    ) as HTMLElement).addEventListener("click", async () => {
+      await fetch(CHECKOUT_ENCRYPTION_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.order),
+      })
+        .then((resp) => {
+          if (resp.ok) {
+            this.toastrService.success("Payment Successful");
+            return resp.json();
+          } else {
+            throw new Error(resp.statusText);
+          }
+        })
+        .then((json) => {
+          console.log(json);
+          window.location.href = json.data;
+        })
+        .catch((error) => {
+          this.toastrService.error("Payment Failed", error);
+          console.log(error);
+        });
+    });
+  }
+
+  ngOnInit() {
+    this.renderTinggButton();
+  }
 }
