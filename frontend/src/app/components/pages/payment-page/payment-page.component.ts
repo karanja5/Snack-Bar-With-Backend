b86ceafcad40667d80ae0af54492428a6d3559c3
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-05-11 16:49:29 +0300

    Updated the payment page

--- frontend/src/app/components/pages/payment-page/payment-page.component.ts
+++ frontend/src/app/components/pages/payment-page/payment-page.component.ts
@@ -12,6 +12,19 @@ import { Router } from "@angular/router";
 export class PaymentPageComponent implements OnInit {
   currentOrder: Order = new Order();
 
+  /**
+   * This function initializes the current order for the current user and redirects to the checkout page
+   * if the cart is empty.
+   * @param {OrderService} orderService - an instance of the OrderService class, which is used to
+   * retrieve the current user's new order.
+   * @param {ToastrService} toastrService - ToastrService is a service provided by the Toastr library
+   * that allows displaying toast notifications in the application. It is used in this constructor to
+   * display an error message if the cart is empty.
+   * @param {Router} router - Router is a service provided by Angular that allows for navigation between
+   * different views or components in a single-page application. It provides methods for navigating to a
+   * specific route, navigating back to the previous route, and more. In this constructor, the Router is
+   * used to navigate to the "/checkout" route if there
+   */
   constructor(
     orderService: OrderService,
     private toastrService: ToastrService,
