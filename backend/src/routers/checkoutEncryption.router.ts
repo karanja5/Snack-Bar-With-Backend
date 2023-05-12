83d74609f907c5bf48e1135c7bbd118f6050a619
Alex Karanja <karanja.munyaka@students.jkuat.ac.ke>
2023-05-12 14:39:17 +0300

    Saved payment details to database

--- backend/src/routers/checkoutEncryption.router.ts
+++ backend/src/routers/checkoutEncryption.router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import Encryption from "../configs/encryption.config";
+import Encryption from "../routers/encryption";
 // import { OrderModel } from "../models/order.model";
 // import { OrderStatus } from "../constants/order_status_enum";
 import asyncHandler from "express-async-handler";
-import { IPayload } from "../models/payload.model";
+import { IPayload, PayloadModel } from "../models/payload.model";
+import { OrderStatus } from "../constants/order_status_enum";
+import { OrderModel } from "../models/order.model";
+import { forEachChild } from "typescript";
 // import { random } from "lodash";
 // import { TRACKING_ORDER_FOR_CURRENT_USER_URL } from "../../../frontend/src/app/shared/constants/urls";
 
@@ -12,7 +15,7 @@ const router = Router();
 const accessKey = process.env.ACCESS_KEY;
 const IVKey = process.env.IV_KEY;
 const secretKey: string = process.env.SECRET_KEY!;
-const algorithm: string = "aes-256-cbc";
+const algorithm: string = process.env.ALGORITHM!;
 
 router.post(
   "/checkoutEncryption",
@@ -22,6 +25,8 @@ router.post(
     let parts = currentOrder.name.split(" ");
     let firstName = parts[0];
     let lastName = parts[1];
+    let itemNames = currentOrder.items.map((item: any) => item.food.name);
+    console.log(itemNames);
     const payload: IPayload = {
       merchantTransactionID: currentOrder._id,
       requestAmount: currentOrder.totalPrice.toString(),
@@ -29,7 +34,7 @@ router.post(
       accountNumber: currentOrder.user,
       serviceCode: process.env.SERVICE_CODE!,
       dueDate: "", //Must be a future date
-      requestDescription: "Dummy merchant transaction",
+      requestDescription: "Payment for " + itemNames.join(", "),
       countryCode: "KE",
       languageCode: "en",
       payerClientCode: "",
@@ -37,7 +42,7 @@ router.post(
       customerFirstName: firstName,
       customerLastName: lastName,
       customerEmail: currentOrder.email,
-      successRedirectUrl: "localhost:4200/track/" + currentOrder._id,
+      successRedirectUrl: "https://localhost:4200/track/" + currentOrder._id,
       failRedirectUrl: "localhost:4200/payment",
       pendingRedirectUrl: "",
       paymentWebhookUrl:
@@ -49,6 +54,9 @@ router.post(
     const params: string = encryption.encrypt(JSON.stringify(payload));
     const URL: string = `https://developer.tingg.africa/checkout/v2/express/?params=${params}&accessKey=${accessKey}&countryCode=${payload.countryCode}`;
 
+    await PayloadModel.create(payload);
+    currentOrder.status = OrderStatus.PENDING;
+    await OrderModel.findByIdAndUpdate(currentOrder._id, currentOrder); //Update the order status to PAID
     res.json({ success: true, data: URL });
   })
 );
